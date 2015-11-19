b36fbfed6611ab1bb88474db9a0e33d24c296fea
detomon <simon@monoxid.ch>
2015-11-19 16:07:40 +0100

    Implement tests

--- test/test.h
+++ test/test.h
@@ -1,5 +1,8 @@
+#include <stdio.h>
+#include <string.h>
 #include "json5.h"
 
 #define RESULT_PASS   0
+#define RESULT_FAIL   1
 #define RESULT_SKIP  77
 #define RESULT_ERROR 99
--- test/test_json5.c
+++ test/test_json5.c
@@ -2,5 +2,20 @@
 
 int main (int argc, char const * argv [])
 {
+	json5_tokenizer tknzr;
+
+	if (json5_tokenizer_init (&tknzr) != 0) {
+		fprintf (stderr, "Failed to initialize tokenizer\n");
+		return RESULT_ERROR;
+	}
+
+	unsigned char const * string = "😐";
+
+	for (unsigned char const * c = string; *c; c ++) {
+		if (json5_tokenizer_put_byte (&tknzr, *c) < 0) {
+			fprintf (stderr, "Failed to put byte: %d\n", *c);
+		}
+	}
+
 	return RESULT_PASS;
 }
